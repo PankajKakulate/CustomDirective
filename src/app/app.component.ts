3008e81262f382818253622d4191576694029441
pankaj <pankaj7187815@gmail.com>
2024-01-04 07:50:37 +0530

    i c 04-01-2024

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { MarvellousDirective } from './marvellous.directive';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,MarvellousDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
